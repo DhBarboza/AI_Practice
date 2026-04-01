38669f82e9afe4b3c1028e16ee47b73c7f771772
Douglas Henrique LINUX <dev.dhbarboza@gmail.com>
2026-03-31 22:24:46 -0300

    feat: process products and customers with cross-referencing`

--- 01-Ex-Product_Recommendation_System/src/workers/modelTrainingWorker.js
+++ 01-Ex-Product_Recommendation_System/src/workers/modelTrainingWorker.js
@@ -1,10 +1,8 @@
 import "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.22.0/dist/tf.min.js";
 import { workerEvents } from "../events/constants.js";
-
-console.log("Model training worker initialized");
 let _globalCtx = {};
+let _model = null;
 
-// Definição de "peso" para recomendação de produtos
 const WEIGHTS = {
     category: 0.4,
     color: 0.3,
@@ -12,28 +10,15 @@ const WEIGHTS = {
     age: 0.1,
 };
 
-// Normalização
-/*
-Normalizar valores contínuos (preço, idade) para o intervalo de 0 a 1
-Por quê?
-Mantém todas as variáveis equilibradas, evitando que uma tenha mais influência que as outras durante o treinamento.
-
-Fórmula:
-(val−min)/(max−min)
-
-Exemplo:
-preço = 129.99
-preço mínimo = 39.99
-preço máximo = 199.99
-
-Resultado ≈ 0,56
-*/
-
+// 🔢 Normalize continuous values (price, age) to 0–1 range
+// Why? Keeps all features balanced so no one dominates training
+// Formula: (val - min) / (max - min)
+// Example: price=129.99, minPrice=39.99, maxPrice=199.99 → 0.56
 const normalize = (value, min, max) => (value - min) / (max - min || 1);
 
-function makeContext(catalog, users) {
+function makeContext(products, users) {
     const ages = users.map((u) => u.age);
-    const prices = catalog.map((p) => p.price);
+    const prices = products.map((p) => p.price);
 
     const minAge = Math.min(...ages);
     const maxAge = Math.max(...ages);
@@ -41,22 +26,22 @@ function makeContext(catalog, users) {
     const minPrice = Math.min(...prices);
     const maxPrice = Math.max(...prices);
 
-    const colors = [...new Set(catalog.map((p) => p.color))];
-    const categories = [...new Set(catalog.map((p) => p.category))];
+    const colors = [...new Set(products.map((p) => p.color))];
+    const categories = [...new Set(products.map((p) => p.category))];
 
-    const colorIndex = Object.fromEntries(
+    const colorsIndex = Object.fromEntries(
         colors.map((color, index) => {
             return [color, index];
         }),
     );
-
     const categoriesIndex = Object.fromEntries(
         categories.map((category, index) => {
             return [category, index];
         }),
     );
 
-    // Computar a média de idade dos compradores por produto (ajuda a personalisar)
+    // Computar a média de idade dos comprados por produto
+    // (ajuda a personalizar)
     const midAge = (minAge + maxAge) / 2;
     const ageSums = {};
     const ageCounts = {};
@@ -68,21 +53,21 @@ function makeContext(catalog, users) {
         });
     });
 
-    // Tranformou os valores entre 0 -> 1
     const productAvgAgeNorm = Object.fromEntries(
-        catalog.map((product) => {
+        products.map((product) => {
             const avg = ageCounts[product.name]
                 ? ageSums[product.name] / ageCounts[product.name]
                 : midAge;
+
             return [product.name, normalize(avg, minAge, maxAge)];
         }),
     );
 
     return {
-        catalog,
+        products,
         users,
+        colorsIndex,
         categoriesIndex,
-        colorIndex,
         productAvgAgeNorm,
         minAge,
         maxAge,
@@ -90,7 +75,7 @@ function makeContext(catalog, users) {
         maxPrice,
         numCategories: categories.length,
         numColors: colors.length,
-        // price + age+ colors + categories
+        // price + age + colors + categories
         dimentions: 2 + categories.length + colors.length,
     };
 }
@@ -99,7 +84,8 @@ const oneHotWeighted = (index, length, weight) =>
     tf.oneHot(index, length).cast("float32").mul(weight);
 
 function encodeProduct(product, context) {
-    // Normalizando os dados para ficar de 0 a 1 e aplicar o peso na recomendação
+    // normalizando dados para ficar de 0 a 1 e
+    // aplicar o peso na recomendação
     const price = tf.tensor1d([
         normalize(product.price, context.minPrice, context.maxPrice) *
             WEIGHTS.price,
@@ -116,7 +102,7 @@ function encodeProduct(product, context) {
     );
 
     const color = oneHotWeighted(
-        context.colorIndex[product.color],
+        context.colorsIndex[product.color],
         context.numColors,
         WEIGHTS.color,
     );
@@ -124,19 +110,59 @@ function encodeProduct(product, context) {
     return tf.concat1d([price, age, category, color]);
 }
 
+function encodeUser(user, context) {
+    if (user.purchases.length) {
+        return tf
+            .stack(
+                user.purchases.map((product) =>
+                    encodeProduct(product, context),
+                ),
+            )
+            .mean(0)
+            .reshape([1, context.dimentions]);
+    }
+}
+
+function createTrainingData(context) {
+    const inputs = [];
+    const labels = [];
+    context.users
+        .filter((u) => u.purchases.length)
+        .forEach((user) => {
+            const userVector = encodeUser(user, context).dataSync();
+            context.products.forEach((product) => {
+                const productVector = encodeProduct(
+                    product,
+                    context,
+                ).dataSync();
+
+                const label = user.purchases.some((purchase) =>
+                    purchase.name === product.name ? 1 : 0,
+                );
+                // combinar user + product
+                inputs.push([...userVector, ...productVector]);
+                labels.push(label);
+            });
+        });
+
+    return {
+        xs: tf.tensor2d(inputs),
+        ys: tf.tensor2d(labels, [labels.length, 1]),
+        inputDimention: context.dimentions * 2,
+        // tamanho = userVector + productVector
+    };
+}
+
 async function trainModel({ users }) {
     console.log("Training model with users:", users);
-
     postMessage({
         type: workerEvents.progressUpdate,
-        progress: { progress: 50 },
+        progress: { progress: 1 },
     });
+    const products = await (await fetch("/data/products.json")).json();
 
-    // Carregar os produtos da base de dados em memória:
-    const catalog = await (await fetch("/data/products.json")).json();
-
-    const context = makeContext(catalog, users);
-    context.productVectors = catalog.map((product) => {
+    const context = makeContext(products, users);
+    context.productVectors = products.map((product) => {
         return {
             name: product.name,
             meta: { ...product },
@@ -144,37 +170,21 @@ async function trainModel({ users }) {
         };
     });
 
-    debugger;
-
     _globalCtx = context;
 
+    const trainData = createTrainingData(context);
+    debugger;
+
     postMessage({
-        type: workerEvents.trainingLog,
-        epoch: 1,
-        loss: 1,
-        accuracy: 1,
+        type: workerEvents.progressUpdate,
+        progress: { progress: 100 },
     });
-
-    setTimeout(() => {
-        postMessage({
-            type: workerEvents.progressUpdate,
-            progress: { progress: 100 },
-        });
-        postMessage({ type: workerEvents.trainingComplete });
-    }, 1000);
+    postMessage({ type: workerEvents.trainingComplete });
 }
-function recommend(user, ctx) {
-    console.log("will recommend for user:", user);
-    // postMessage({
-    //     type: workerEvents.recommend,
-    //     user,
-    //     recommendations: []
-    // });
-}
-
+function recommend({ user }) {}
 const handlers = {
     [workerEvents.trainModel]: trainModel,
-    [workerEvents.recommend]: (d) => recommend(d.user, _globalCtx),
+    [workerEvents.recommend]: recommend,
 };
 
 self.onmessage = (e) => {
